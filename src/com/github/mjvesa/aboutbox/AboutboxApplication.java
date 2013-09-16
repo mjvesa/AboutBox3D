78725bb8465010e40cfd0f27e9296b4ad8a14318
Matti Vesa <mjvesa@vaadin.com>
2013-09-16 11:52:31 +0300

    Format example program

--- src/com/github/mjvesa/aboutbox/AboutboxApplication.java
+++ src/com/github/mjvesa/aboutbox/AboutboxApplication.java
@@ -12,37 +12,49 @@ import com.vaadin.ui.VerticalLayout;
 @Theme("aboutbox")
 public class AboutboxApplication extends UI {
 
-	private static final long serialVersionUID = 5853529673557124301L;
+    private static final long serialVersionUID = 5853529673557124301L;
 
-	@Override
-	protected void init(VaadinRequest request) {
-		getPage().setTitle("AboutBox demo");
-		
-		final VerticalLayout vl = new VerticalLayout();
-		vl.addComponent(new Button("Open AboutBox", new Button.ClickListener() {
-			
-			private static final long serialVersionUID = 5405789614881134772L;
+    @Override
+    protected void init(VaadinRequest request) {
+        getPage().setTitle("AboutBox demo");
 
-			@Override
-			public void buttonClick(ClickEvent event) {
-				AboutBox ab = new AboutBox();
-				ab.setWidth("400px");
-				ab.setHeight("400px");
-				ab.setLogo(new ThemeResource("img/vaadin.png"));
-				ab.setCreditsHTML("<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>" +
-						"<br/><br/><br/><br/><br/><h1>Credits</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum <a href=\"http://vaadin.com\" target=\"_blank\">w0rd.</a><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>");
-				ab.setScrollLength(400);
-				ab.setFooterHTML("<h3>Footer</h3>");
-				ab.setEffect(Effect.LOGO3D);
-				getUI().addWindow(ab);
-				ab.setTessellationAccuracy(30, 30, 0.6, 0.50);
-				ab.getAnimatedBackground().setEdgeAAEnabled(false);
-				ab.getAnimatedBackground().setTemporalAAEnabled(false);
-				ab.center();
-				
-			}
-		}));
-		setContent(vl);
-		
-	}
+        final VerticalLayout vl = new VerticalLayout();
+        vl.addComponent(new Button("Open AboutBox",
+                new Button.ClickListener() {
+
+                    private static final long serialVersionUID = 5405789614881134772L;
+
+                    @Override
+                    public void buttonClick(ClickEvent event) {
+                        AboutBox ab = new AboutBox();
+                        ab.setWidth("400px");
+                        ab.setHeight("400px");
+                        ab.setLogo(new ThemeResource("img/vaadin.png"));
+                        ab.setCreditsHTML("<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>"
+                                + "<br/><br/><br/><br/><br/><h1>Credits</h1><p>Lorem ipsum dolor sit" +
+                                " amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut" +
+                                " labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud" +
+                                " exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+                                " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum" +
+                                " dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non" +
+                                " proident, sunt in culpa qui officia deserunt mollit anim id est laborum" +
+                                " <a href=\"http://vaadin.com\" target=\"_blank\">w0rd." +
+                                "</a><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>" +
+                                "<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>" +
+                                "<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>");
+                        ab.setScrollLength(400);
+                        ab.setFooterHTML("<h3>Footer</h3>");
+                        ab.setEffect(Effect.LOGO3D);
+                        getUI().addWindow(ab);
+                        ab.setTessellationAccuracy(30, 30, 0.6, 0.50);
+                        ab.getAnimatedBackground().setEdgeAAEnabled(false);
+                        ab.getAnimatedBackground()
+                                .setTemporalAAEnabled(false);
+                        ab.center();
+
+                    }
+                }));
+        setContent(vl);
+
+    }
 }
